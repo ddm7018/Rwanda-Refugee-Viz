1eb7db1d4b2197d831002b8cd99660b9af216cae
Daniel Mooney <ddm7018@rit.edu>
2017-07-06 14:48:41 -0400

    adding run models page

--- ui.R
+++ ui.R
@@ -1,24 +1,24 @@
 library(leaflet)
 
 # Choices for drop-downs
-color_vars <- colnames(rwanda)
+#color_vars <- colnames(rwanda)
 
-removeFrom = c("object_id","global_id","row_id","parent_row_id","x", "y","ObjectID","GlobalID")
-for(ele in removeFrom){ 
-  color_vars <- color_vars[color_vars != ele]
-  }
+#removeFrom = c("object_id","global_id","row_id","parent_row_id","x", "y","ObjectID","GlobalID")
+#for(ele in removeFrom){ 
+#  color_vars <- color_vars[color_vars != ele]
+#  }
 
-vars <- c(
-  "num_employee",
-  "avg_customers"
-)
+vars <- c("camp_name","num_employee","market_condition","market_security","cash_food_local", 
+          "outside_job","competition", "income_compare","business_start", "customer_locations", "customer_locations_camp_change",
+          "entrepreneurship_training", "training_grow", "business_leave_camp" ,"leave_camp_support_business", "id_problem_fequency",
+          "key_good_demand_change","avg_customers","x", "y")
 
+algos <- c("Suppor Vector Machines", "Decision Trees") 
 
 navbarPage("Rwanda", id="nav",
 
   tabPanel("Interactive map",
     div(class="outer",
-
       tags$head(
         # Include our custom CSS
         includeCSS("styles.css"),
@@ -30,13 +30,12 @@ navbarPage("Rwanda", id="nav",
       absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
         draggable = TRUE, top = 60, left = "20", right = "auto", bottom = "auto",
         width = 330, height = "auto",
-
         h2("Rwanda explorer"),
       
-        selectInput("color", "Color", color_vars),
+        selectInput("color", "Color", vars),
         conditionalPanel(
           condition = "input.static_size == false",
-          selectInput("size", "Size", vars)),
+          selectInput("size", "Size", vars, selected = "num_employee")),
         actionButton("change", "Change map"),
         downloadLink("download", "Download"),
         checkboxInput("static_size", "Static Radius", FALSE),
@@ -44,10 +43,18 @@ navbarPage("Rwanda", id="nav",
         condition = "input.static_size == true",
         sliderInput("slider1", label = h3("Slider"), min = 1, 
                     max = 20, value = 5))
-      )
-      
-      
-    )
-  ))
+      ))),
+    tabPanel("Run Models",
+             sidebarPanel(id = "controls", class = "panel panel-default",
+                          selectInput("algo", "Algorithm", algos),
+                          selectInput("DV", "Dependent Variables", vars),
+                          checkboxInput("all_variables", "All Variables Included", FALSE),
+                          conditionalPanel(
+                            condition = "input.all_variables == false",
+                          checkboxGroupInput("IV", "Independent Variables", vars)
+                          ),
+                          actionButton("run_models", "Run Models")
+                          ),textOutput("text1"))
+                )
 
 
